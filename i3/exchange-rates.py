938378ed6f017637381f21e8efb70e2a76954f5c
Ertuğrul Seyhan <4480721+ertseyhan@users.noreply.github.com>
2017-11-12 18:55:02 +0300

    [py3status] removed comment lines from custom module

--- i3/exchange-rates.py
+++ i3/exchange-rates.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Example module that says 'Hello World!'
-
-This demonstrates how to produce a simple custom module.
-"""
-
 from forex_python.bitcoin   import BtcConverter
 from forex_python.converter import CurrencyRates
 
